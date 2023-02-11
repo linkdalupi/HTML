4c23298b0d80e870ce33daa1ea82cf903488eba1
linka <ilincnedel2710@student.lvusd.org>
2023-02-11 12:01:25 -0800

    orez cu lapte secipie

---
+++ lambda.js
@@ -0,0 +1,4 @@
+export const handler = async(event) => {
+    const message = 'You take 1 cup of rice and 1 cup of water. You then grate the skin of an orange. You mix these up and boil it. Add 1 spoon of sugar. When done, season with cinammon. '
+    return message;
+};
